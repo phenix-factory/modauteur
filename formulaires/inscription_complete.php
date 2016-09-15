5ab1d4d589e5da501ebb8cb87b8c2a1a542e1d75
Debondt Didier <p@henix.be>
2016-09-15 15:08:08 +0200

    Début du formulaire d'inscription complete

---
+++ formulaires/inscription_complete.php
@@ -0,0 +1,45 @@
+<?php
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+function formulaires_inscription_complete_saisies_dist() {
+
+    return $saisies;
+}
+
+function formulaires_inscription_complete_charger_dist() {
+    // Contexte du formulaire.
+    $contexte = array(
+        '' => '',
+    );
+
+    return $contexte;
+}
+
+/*
+ *   Fonction de vérification, cela fonction avec un tableau d'erreur.
+ *   Le tableau est formater de la sorte:
+ *   if (!_request('NomErreur')) {
+ *       $erreurs['message_erreur'] = '';
+ *       $erreurs['NomErreur'] = '';
+ *   }
+ *   Pensez à utiliser _T('info_obligatoire'); pour les éléments obligatoire.
+ */
+function formulaires_inscription_complete_verifier_dist() {
+    $erreurs = array();
+
+    return $erreurs;
+}
+
+function formulaires_inscription_complete_traiter_dist() {
+    //Traitement du formulaire.
+
+    // Donnée de retour.
+    return array(
+        'editable' => true,
+        'message_ok' => '',
+        'redirect' => ''
+    );
+}

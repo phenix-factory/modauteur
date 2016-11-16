d6e0fae216ce85f746538fc99d4f26915c71db69
Debondt Didier <p@henix.be>
2016-11-16 12:19:58 +0100

    PSR

--- formulaires/modifier_compte.php
+++ formulaires/modifier_compte.php
@@ -11,64 +11,64 @@ include_spip('inc/session');
 function formulaires_modifier_compte_saisies_dist($id_auteur = null) {
 
 	// Le formulaire de base sans les saisies
-    $saisies = array(
-        array(
-            'saisie' => 'input',
-            'options' => array(
-                'nom' => 'nom',
-                'label' => _T('entree_nom_pseudo')
-            )
-        ),
-        array(
-            'saisie' => 'input',
-            'options' => array(
-                'nom' => 'email',
-                'label' => _T('entree_adresse_email')
-            ),
-            'verifier' => array(
-                'type' => 'email',
-                'options' => array(
-                    'disponible' => true,
-                    'id_auteur' => session_get('id_auteur')
-                )
-            )
-        ),
-        array(
-            'saisie' => 'textarea',
-            'options' => array(
-                'nom' => 'bio',
-                'explication' => _T('entree_biographie'),
-                'label' => _T('entree_infos_perso'),
-                'class' => 'inserer_barre_forum',
-                'rows' => 3
-            )
-        ),
-        array(
-            'saisie' => 'fieldset',
-            'options' => array(
-                'nom' => 'danger_zone',
-                'label' => _T('entree_nouveau_passe')
-            ),
-            'saisies' => array(
-                array(
-                    'saisie' => 'input',
-                    'options' => array(
-                        'nom' => 'new_pass',
-                        'type' => 'password',
-                        'label' => _T('entree_nouveau_passe')
-                    )
-                ),
-                array(
-                    'saisie' => 'input',
-                    'options' => array(
-                        'nom' => 'new_pass2',
-                        'type' => 'password',
-                        'label' => _T('info_confirmer_passe')
-                    )
-                )
-            )
-        )
-    );
+	$saisies = array(
+		array(
+			'saisie' => 'input',
+			'options' => array(
+				'nom' => 'nom',
+				'label' => _T('entree_nom_pseudo')
+			)
+		),
+		array(
+			'saisie' => 'input',
+			'options' => array(
+				'nom' => 'email',
+				'label' => _T('entree_adresse_email')
+			),
+			'verifier' => array(
+				'type' => 'email',
+				'options' => array(
+					'disponible' => true,
+					'id_auteur' => session_get('id_auteur')
+				)
+			)
+		),
+		array(
+			'saisie' => 'textarea',
+			'options' => array(
+				'nom' => 'bio',
+				'explication' => _T('entree_biographie'),
+				'label' => _T('entree_infos_perso'),
+				'class' => 'inserer_barre_forum',
+				'rows' => 3
+			)
+		),
+		array(
+			'saisie' => 'fieldset',
+			'options' => array(
+				'nom' => 'danger_zone',
+				'label' => _T('entree_nouveau_passe')
+			),
+			'saisies' => array(
+				array(
+					'saisie' => 'input',
+					'options' => array(
+						'nom' => 'new_pass',
+						'type' => 'password',
+						'label' => _T('entree_nouveau_passe')
+					)
+				),
+				array(
+					'saisie' => 'input',
+					'options' => array(
+						'nom' => 'new_pass2',
+						'type' => 'password',
+						'label' => _T('info_confirmer_passe')
+					)
+				)
+			)
+		)
+	);
 
 	// Support du plugin champs extras
 	$plugin = chercher_filtre('info_plugin');
@@ -80,7 +80,7 @@ function formulaires_modifier_compte_saisies_dist($id_auteur = null) {
 		$saisies = spip_array_insert($saisies, 0, $champs_extras);
 	}
 
-    return $saisies;
+	return $saisies;
 }
 
 
@@ -92,35 +92,35 @@ function formulaires_modifier_compte_charger_dist($id_auteur = null) {
 
 	// Ne pas charger le formulaire si on a pas le droit de le modifier.
 	if (!autoriser('modifier', 'auteur', intval($id_auteur))) {
-        return false;
+		return false;
 	}
 
-    // On va charger la fonction du formulaire editer_auteur
-    // Le but de la fonction étant le meme
-    $formulaire_editer_auteur = charger_fonction('editer_auteur_charger', 'formulaires');
-    $valeurs = $formulaire_editer_auteur($id_auteur);
+	// On va charger la fonction du formulaire editer_auteur
+	// Le but de la fonction étant le meme
+	$formulaire_editer_auteur = charger_fonction('editer_auteur_charger', 'formulaires');
+	$valeurs = $formulaire_editer_auteur($id_auteur);
 
-    return $valeurs;
+	return $valeurs;
 }
 
 function formulaires_modifier_compte_traiter_dist($id_auteur = null) {
 
-    // on récupère l'id_auteur de la session
+	// on récupère l'id_auteur de la session
 	if (is_null($id_auteur)) {
 		$id_auteur = session_get('id_auteur');
 	}
 
-    // On va charger la fonction du formulaire editer_auteur
-    // Le but de la fonction étant le meme
-    $formulaire_editer_auteur = charger_fonction('editer_auteur_traiter', 'formulaires');
-    $res = $formulaire_editer_auteur($id_auteur);
+	// On va charger la fonction du formulaire editer_auteur
+	// Le but de la fonction étant le meme
+	$formulaire_editer_auteur = charger_fonction('editer_auteur_traiter', 'formulaires');
+	$res = $formulaire_editer_auteur($id_auteur);
 
-    // Pas de redirection
-    $res['redirect'] = '';
+	// Pas de redirection
+	$res['redirect'] = '';
 
 	// On peu éditer le profile
-    $res['editable'] = true;
+	$res['editable'] = true;
 
-    // Donnée de retour.
-    return $res;
+	// Donnée de retour.
+	return $res;
 }
